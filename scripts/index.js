1ed7b851e7afe81e69bc46050b898db191f48fac
Maskim Leskin <maskim.leskin@methed.ru>
2022-09-12 00:51:55 +0300

    3

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ new SimpleBar(document.querySelector('.country__list'), {
   classNames: {
     scrollbar: 'country__scrollbar',
     track: 'country__track',
-  }
+  },
 });
 
 new Swiper('.goods__block', {
@@ -31,7 +31,7 @@ new Swiper('.goods__block', {
     nextEl: '.goods__arrow_next',
   },
   preventClicks: true,
-  a11y: false
+  a11y: false,
 });
 
 const productMore = document.querySelectorAll('.product__more');
@@ -46,7 +46,7 @@ productMore.forEach((btn) => {
 modal.addEventListener('click', (event) => {
   if (event.target === modal) {
     modal.classList.remove('modal_open');
-  }  
+  }
 });
 
 const formPlaceholder = document.querySelectorAll('.form__placeholder');
@@ -64,9 +64,56 @@ formInput.forEach((item, i) => {
   });
 });
 
+const dataCurrency = {};
+
+
+
+const formatCurrency = (value, currency) =>
+  new Intl.NumberFormat('EP', {
+    style: 'currency',
+    currency,
+    maximumFractionDigits: 2,
+  }).format(value);
+
+const showPrice = (currency = 'USD') => {
+  const priceElems = document.querySelectorAll('[data-price]');
+
+  priceElems.forEach((elem) => {
+    elem.textContent = formatCurrency(
+      elem.dataset.price * dataCurrency[currency],
+      currency,
+    );
+  });
+};
+
+const myHeaders = new Headers();
+myHeaders.append('apikey', 'cOVfoZlwc2Jnv5xL1HV8dUCFwiFFKTRb');
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders,
+};
+
+fetch('https://api.apilayer.com/fixer/latest?base=USD', requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log('result: ', result);
+    Object.assign(dataCurrency, result.rates);
+    showPrice();
+  })
+  .catch((error) => console.log('error', error));
+
 const countryBtn = document.querySelector('.country__btn');
 const countryWrapper = document.querySelector('.country__wrapper');
 
 countryBtn.addEventListener('click', () => {
   countryWrapper.classList.toggle('country__wrapper_open');
-})
+});
+
+countryWrapper.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('country__choise')) {
+    countryWrapper.classList.remove('country__wrapper_open');
+    showPrice(target.dataset.currency);
+  }
+});
