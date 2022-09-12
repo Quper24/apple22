086c96791c4b7d4a9345e71552a5eacd02e75b70
Maskim Leskin <maskim.leskin@methed.ru>
2022-09-12 18:02:50 +0300

    day4ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,16 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
+
+// simplebar
+
 new SimpleBar(document.querySelector('.country__list'), {
   classNames: {
     scrollbar: 'country__scrollbar',
-    track: 'country__track',
-  },
-});
+    track: 'country__track'
+  }
+})
 
+// slider
 new Swiper('.goods__block', {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -15,7 +19,7 @@ new Swiper('.goods__block', {
       slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     1024: {
       slidesPerView: 2,
@@ -28,90 +32,96 @@ new Swiper('.goods__block', {
   },
   navigation: {
     prevEl: '.goods__arrow_prev',
-    nextEl: '.goods__arrow_next',
+    nextEl: '.goods__arrow_next'
   },
   preventClicks: true,
   a11y: false,
 });
+
+// modal
 
 const productMore = document.querySelectorAll('.product__more');
 const modal = document.querySelector('.modal');
 
 productMore.forEach((btn) => {
   btn.addEventListener('click', () => {
-    modal.classList.add('modal_open');
-  });
+    modal.classList.add('modal_open')
+  })
 });
 
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.classList.remove('modal_open');
+modal.addEventListener('click', ({target}) => {
+  if (target === modal) {
+    modal.classList.remove('modal_open')
   }
 });
 
-const formPlaceholder = document.querySelectorAll('.form__placeholder');
+const formPlaceloder = document.querySelectorAll('.form__placeholder');
 const formInput = document.querySelectorAll('.form__input');
 
-formInput.forEach((item, i) => {
-  item.addEventListener('focus', () => {
-    formPlaceholder[i].classList.add('form__placeholder_active');
-  });
+formInput.forEach((input, i) => {
+  input.addEventListener('focus', () => {
+    formPlaceloder[i].classList.add('form__placeholder_active')
+  })
 
-  item.addEventListener('blur', () => {
-    if (item.value === '') {
-      formPlaceholder[i].classList.remove('form__placeholder_active');
+  input.addEventListener('blur', () => {
+    if (input.value === '') {
+      formPlaceloder[i].classList.remove('form__placeholder_active')
     }
-  });
+  })
 });
+
+
+
+
+// currency
 
 const dataCurrency = {};
 
-
-
-const formatCurrency = (value, currency) =>
-  new Intl.NumberFormat('EP', {
+const formatCurrency = (value, currency) => {
+  return new Intl.NumberFormat('EU', {
     style: 'currency',
     currency,
     maximumFractionDigits: 2,
-  }).format(value);
+  }).format(value)
+}
 
 const showPrice = (currency = 'USD') => {
   const priceElems = document.querySelectorAll('[data-price]');
 
-  priceElems.forEach((elem) => {
-    elem.textContent = formatCurrency(
-      elem.dataset.price * dataCurrency[currency],
-      currency,
-    );
-  });
-};
+  priceElems.forEach(elem => {
+    elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency] , currency);
+  })
+}
+
 
 const myHeaders = new Headers();
-myHeaders.append('apikey', 'cOVfoZlwc2Jnv5xL1HV8dUCFwiFFKTRb');
+myHeaders.append("apikey", "J5gtDFgL5KbjBrQKi74XKL3fCetO27Fn");
 
 const requestOptions = {
   method: 'GET',
   redirect: 'follow',
-  headers: myHeaders,
+  headers: myHeaders
 };
 
-fetch('https://api.apilayer.com/fixer/latest?base=USD', requestOptions)
-  .then((response) => response.json())
-  .then((result) => {
-    console.log('result: ', result);
-    Object.assign(dataCurrency, result.rates);
+fetch("https://api.apilayer.com/fixer/latest?base=USD", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    Object.assign(dataCurrency, result.rates)
     showPrice();
   })
-  .catch((error) => console.log('error', error));
+  .catch(error => console.log('error', error));
+
+
+// choices
 
 const countryBtn = document.querySelector('.country__btn');
 const countryWrapper = document.querySelector('.country__wrapper');
 
 countryBtn.addEventListener('click', () => {
-  countryWrapper.classList.toggle('country__wrapper_open');
+  countryWrapper.classList.toggle('country__wrapper_open')
 });
 
-countryWrapper.addEventListener('click', ({ target }) => {
+countryWrapper.addEventListener('click',  ({target}) => {
   if (target.classList.contains('country__choise')) {
     countryWrapper.classList.remove('country__wrapper_open');
     showPrice(target.dataset.currency);
