f023ece68cb58725058863d26856f53a34033f19
Maksim Leskin <maksim.leskin@methed.ru>
2022-09-13 15:41:23 +0300

    ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
 import Swiper from '../lib/swiper-bundle.esm.browser.min.js';
 
-
 // simplebar
 
 new SimpleBar(document.querySelector('.country__list'), {
   classNames: {
     scrollbar: 'country__scrollbar',
-    track: 'country__track'
-  }
-})
+    track: 'country__track',
+  },
+});
 
 // slider
 new Swiper('.goods__block', {
@@ -19,7 +18,7 @@ new Swiper('.goods__block', {
       slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
       slidesPerView: 2,
@@ -32,7 +31,7 @@ new Swiper('.goods__block', {
   },
   navigation: {
     prevEl: '.goods__arrow_prev',
-    nextEl: '.goods__arrow_next'
+    nextEl: '.goods__arrow_next',
   },
   preventClicks: true,
   a11y: false,
@@ -45,13 +44,13 @@ const modal = document.querySelector('.modal');
 
 productMore.forEach((btn) => {
   btn.addEventListener('click', () => {
-    modal.classList.add('modal_open')
-  })
+    modal.classList.add('modal_open');
+  });
 });
 
-modal.addEventListener('click', ({target}) => {
+modal.addEventListener('click', ({ target }) => {
   if (target === modal) {
-    modal.classList.remove('modal_open')
+    modal.classList.remove('modal_open');
   }
 });
 
@@ -60,18 +59,15 @@ const formInput = document.querySelectorAll('.form__input');
 
 formInput.forEach((input, i) => {
   input.addEventListener('focus', () => {
-    formPlaceloder[i].classList.add('form__placeholder_active')
-  })
+    formPlaceloder[i].classList.add('form__placeholder_active');
+  });
 
   input.addEventListener('blur', () => {
     if (input.value === '') {
-      formPlaceloder[i].classList.remove('form__placeholder_active')
+      formPlaceloder[i].classList.remove('form__placeholder_active');
     }
-  })
+  });
 });
-
-
-
 
 // currency
 
@@ -82,35 +78,36 @@ const formatCurrency = (value, currency) => {
     style: 'currency',
     currency,
     maximumFractionDigits: 2,
-  }).format(value)
-}
+  }).format(value);
+};
 
 const showPrice = (currency = 'USD') => {
   const priceElems = document.querySelectorAll('[data-price]');
 
-  priceElems.forEach(elem => {
-    elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency] , currency);
-  })
-}
-
+  priceElems.forEach((elem) => {
+    elem.textContent = formatCurrency(
+      elem.dataset.price * dataCurrency[currency],
+      currency,
+    );
+  });
+};
 
 const myHeaders = new Headers();
-myHeaders.append("apikey", "J5gtDFgL5KbjBrQKi74XKL3fCetO27Fn");
+myHeaders.append('apikey', 'J5gtDFgL5KbjBrQKi74XKL3fCetO27Fn');
 
 const requestOptions = {
   method: 'GET',
   redirect: 'follow',
-  headers: myHeaders
+  headers: myHeaders,
 };
 
-fetch("https://api.apilayer.com/fixer/latest?base=USD", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-    Object.assign(dataCurrency, result.rates)
+fetch('https://api.apilayer.com/fixer/latest?base=USD', requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+    Object.assign(dataCurrency, result.rates);
     showPrice();
   })
-  .catch(error => console.log('error', error));
-
+  .catch((error) => console.log('error', error));
 
 // choices
 
@@ -118,12 +115,60 @@ const countryBtn = document.querySelector('.country__btn');
 const countryWrapper = document.querySelector('.country__wrapper');
 
 countryBtn.addEventListener('click', () => {
-  countryWrapper.classList.toggle('country__wrapper_open')
+  countryWrapper.classList.toggle('country__wrapper_open');
 });
 
-countryWrapper.addEventListener('click',  ({target}) => {
+countryWrapper.addEventListener('click', ({ target }) => {
   if (target.classList.contains('country__choise')) {
     countryWrapper.classList.remove('country__wrapper_open');
     showPrice(target.dataset.currency);
   }
 });
+
+const declOfNum = (n, titles) => titles[n % 10 === 1 && n % 100 !== 11 ?
+  0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+const timer = (deadline) => {
+  const unitDay = document.querySelector('.timer__unit_day');
+  const unitHour = document.querySelector('.timer__unit_hour');
+  const unitMin = document.querySelector('.timer__unit_min');
+  const descriptionDay = document.querySelector('.timer__unit-description_day');
+  const descriptionHour = document.querySelector('.timer__unit-description_hour');
+  const descriptionMin = document.querySelector('.timer__unit-description_min');
+
+  const getTimeRemaining = () => {
+    const dateStop = new Date(deadline).getTime();
+    const dateNow = Date.now();
+    const timeRemaining = dateStop - dateNow;
+
+    const minutes = Math.floor((timeRemaining / 1000 / 60) % 60);
+    const hours = Math.floor((timeRemaining / 1000 / 60 / 60) % 24);
+    const days = Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
+
+    return { timeRemaining, minutes, hours, days };
+  };
+
+  const start = () => {
+    const timer = getTimeRemaining();
+
+    unitDay.textContent = timer.days;
+    unitHour.textContent = timer.hours;
+    unitMin.textContent = timer.minutes;
+
+    descriptionDay.textContent = declOfNum(timer.days, ['день', 'дня', 'дней']);
+    descriptionHour.textContent = declOfNum(timer.hours, ['час','часа','часов']);
+    descriptionMin.textContent = declOfNum(timer.minutes, ['минута', 'минуты', 'минут']);
+
+    const intervalId = setTimeout(start, 60000);
+    if (timer.timeRemaining < 0) {
+      clearTimeout(intervalId);
+      unitDay.textContent = '0';
+      unitHour.textContent = '0';
+      unitMin.textContent = '0';
+    }
+  };
+
+  start();
+};
+
+timer('2023/09/07 20:00');
